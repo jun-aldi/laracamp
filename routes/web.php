e0645e0c0150ffd4a4cec9d595f5dc03fb136e0e
juniaraldi <aldinugraha708@gmail.com>
2024-05-29 02:03:00 +0700

    Validation Payment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +20,26 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name('checkout');
 
-Route::get('/success_checkout', function () {
-    return view('success_checkout');
-})->name('success_checkout');
+
+
+
+
+//socialite
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'] )->name('user.google.callback');
+
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
+
+
+
+require __DIR__.'/auth.php';
+
