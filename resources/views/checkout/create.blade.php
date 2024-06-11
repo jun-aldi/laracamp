9d698900cf620062d0f705a0be5e562a9b422128
juniaraldi <aldinugraha708@gmail.com>
2024-06-11 14:23:27 +0700

    Update Checkout Page

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -61,33 +61,24 @@
                                     @endif
                                 </div>
                                 <div class="mb-4">
-                                    <label class="form-label">Card Number</label>
-                                    <input name="card_number" type="number" required
-                                        class="form-control {{ $errors->has('card_number') ? 'is_invalid' : '' }} "
-                                        aria-describedby="emailHelp" value="{{ old('card_number') ?: '' }}">
-                                    @if ($errors->has('card_number'))
-                                        <p class="text-danger">{{ $errors->first('card_number') }}</p>
+                                    <label class="form-label">Phone</label>
+                                    <input required name="phone" type="text"
+                                        class="form-control {{ $errors->has('phone') ? 'is_invalid' : '' }}"
+                                        aria-describedby="emailHelp" {{-- ?: adalah 'kalau gk ada' --}}
+                                        value="{{ old('phone') ?: Auth::user()->phone }}">
+                                    @if ($errors->has('phone'))
+                                        <p class="text-danger">{{ $errors->first('phone') }}</p>
                                     @endif
                                 </div>
-                                <div class="mb-5">
-                                    <div class="row">
-                                        <div class="col-lg-6 col-12">
-                                            <label class="form-label">Expired</label>
-                                            <input required name="expired" type="month" class="form-control {{ $errors->has('expired') ? 'is_invalid' : '' }}"
-                                                aria-describedby="emailHelp" value="{{ old('expired') ?: '' }}">
-                                            @if ($errors->has('expired'))
-                                                <p class="text-danger">{{ $errors->first('expired') }}</p>
-                                            @endif
-                                        </div>
-                                        <div class="col-lg-6 col-12">
-                                            <label class="form-label">CVC</label>
-                                            <input required name="cvc" maxlength="3" type="number" class="form-control {{ $errors->has('cvc') ? 'is_invalid' : '' }}"
-                                                aria-describedby="emailHelp" value="{{ old('cvc') ?: '' }}">
-                                                @if ($errors->has('cvc'))
-                                                <p class="text-danger">{{ $errors->first('cvc') }}</p>
-                                            @endif
-                                        </div>
-                                    </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Address</label>
+                                    <input required name="address" type="text"
+                                        class="form-control {{ $errors->has('address') ? 'is_invalid' : '' }}"
+                                        aria-describedby="emailHelp" {{-- ?: adalah 'kalau gk ada' --}}
+                                        value="{{ old('address') ?: Auth::user()->address }}">
+                                    @if ($errors->has('address'))
+                                        <p class="text-danger">{{ $errors->first('address') }}</p>
+                                    @endif
                                 </div>
                                 <button type="submit" class="w-100 btn btn-primary">Pay Now</button>
                                 <p class="mt-4 text-center subheader">
